1c59cb9273740e9c53f2378eb99278114725d15a
Mostafa Izadloo <60775213+MoIzadloo@users.noreply.github.com>
2024-01-19 11:32:51 +0330

    :sparkles: feat: update job.py

--- job.py
+++ job.py
@@ -7,14 +7,12 @@ import re
 import time
 
 class SMSSpamDetection(MRJob):
-    OUTPUT_PROTOCOL = JSONValueProtocol
-    def preprocess_message(self, message):
-        message = message.lower()
-        message = re.sub(r'[^\w\s]', '', message)
-        message = re.sub(r'\s+', ' ', message)
-        return message
 
-    def mapper_init(self):
+    OUTPUT_PROTOCOL = JSONValueProtocol
+
+    def __init__(self, *args, **kwargs):
+        MRJob.__init__(self, *args, **kwargs)
+        self.start_time = time.time()
         data = pd.read_csv('spam.csv', encoding = "ISO-8859-1")
         with open('model.pkl', 'rb') as f:
             self.model = pickle.load(f)
@@ -23,33 +21,40 @@ class SMSSpamDetection(MRJob):
         self.vectorizer = CountVectorizer()
         self.vectorizer.fit_transform(preprocessed_messages)
 
+    def preprocess_message(self, message):
+        message = message.lower()
+        message = re.sub(r'[^\w\s]', '', message)
+        message = re.sub(r'\s+', ' ', message)
+        return message     
+
     def mapper(self, _, message):
-        current_time = time.time()
         preprocessed_message = self.preprocess_message(message)
         features = self.vectorizer.transform([preprocessed_message])
         proba = self.model.predict_proba(features).tolist()
-        yield "messages", [list(proba[0]), current_time, message]
+        yield "messages", [list(proba[0]), message]
 
     def reducer(self, key, values):
-        output = []
+        messages = []
         for value in values: 
             record = value
-            current_time = time.time()
             label = "spam"
             proba = record[0]
-            mapTime = record[1]
             if proba[0] < proba[1]:
                 label = "spam"
             else:
                 label = "ham"
-            output.append({
-                "message": record[2],
+            messages.append({
+                "message": record[1],
                 "label": label,
-                "time": "{0:.2f}s".format(current_time - mapTime)
             })
+        output = {
+            "messages": messages,
+            "metaData": {
+                "totalMessages": len(messages),
+                "time": "{0:.3f}s".format(time.time() - self.start_time)
+            }
+        }
         yield key, output
         
-        
-
 if __name__ == '__main__':
     SMSSpamDetection.run()
